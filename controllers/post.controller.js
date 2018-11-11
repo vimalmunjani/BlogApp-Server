54e1111050d2da1168a9d93caf56e6f267403797
Vimal Munjani <vimal.j.munjani@gmail.com>
2018-11-11 22:01:47 +0530

    updated post controller

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,21 +1,45 @@
+let data = [
+    {
+        title: 'First Post',
+        content: 'I am first post'
+    },
+    {
+        title: 'Second Post',
+        content: 'I am Second post'
+    },
+    {
+        title: 'Third Post',
+        content: 'I am Third post'
+    }
+];
+
 exports.getPost = (req, res) => {
 
     res.status(200).json({
         status: 200,
         message: 'Get Post Successful',
-        data: null
+        data: data
     });
 
 }
 
 exports.addPost = (req, res) => {
 
+    let post = req.body;
+    data.push(post);
+    console.log('All post', data);
+    res.status(201).json('Post received');
+
 }
 
 exports.editPost = (req, res) => {
 
+
+
 }
 
 exports.deletePost = (req, res) => {
+
+
 
 }
